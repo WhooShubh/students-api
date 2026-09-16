4f73f850c7254bacb5fb9ce04e254fa9563102f7
Shubhansh Srivastava <srivastavashubhansh01@gmail.com>
2026-09-16 14:11:32 +0530

    Add students API implementation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,14 +1,67 @@
 package main
 
-import "github.com/WhooShubh/students-api/internal/config"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/WhooShubh/students-api/internal/config"
+	"github.com/WhooShubh/students-api/internal/http/handlers/student"
+	"github.com/WhooShubh/students-api/internal/storage/sqlite"
+)
 
 func main() {
 	//load config
 	cfg := config.MustLoad()
 
-	//database setip
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//setup router
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
 
-	//setup server
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /api/students", student.New(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students", student.GetList(storage))
+	// setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
 }
